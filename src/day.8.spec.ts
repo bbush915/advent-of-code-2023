ac4c1f6f0fb346fb0703556e06247fe901327235
bbush915 <bushb@umich.edu>
2023-12-07 23:38:44 -0600

    feat: Day 8

---
+++ src/day.8.spec.ts
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+import { part1, part2 } from "./day.8";
+
+describe("Day 8", function () {
+  afterEach(function () {
+    jest.restoreAllMocks();
+  });
+
+  describe("Part 1", function () {
+    it("should calculate the correct answer for the first example", function () {
+      const input = fs.readFileSync("./src/day.8.example.1.txt");
+      jest.spyOn(fs, "readFileSync").mockImplementation(() => input);
+
+      const answer = part1();
+
+      expect(answer).toBe(2);
+    });
+
+    it("should calculate the correct answer for the first example", function () {
+      const input = fs.readFileSync("./src/day.8.example.2.txt");
+      jest.spyOn(fs, "readFileSync").mockImplementation(() => input);
+
+      const answer = part1();
+
+      expect(answer).toBe(6);
+    });
+
+    it("should calculate the correct answer for the challenge", function () {
+      const answer = part1();
+      expect(answer).toBe(18673);
+    });
+  });
+
+  describe("Part 2", function () {
+    it("should calculate the correct answer for the first example", function () {
+      const input = fs.readFileSync("./src/day.8.example.3.txt");
+      jest.spyOn(fs, "readFileSync").mockImplementation(() => input);
+
+      const answer = part2();
+
+      expect(answer).toBe(6);
+    });
+
+    it("should calculate the correct answer for the challenge", function () {
+      const answer = part2();
+      expect(answer).toBe(17972669116327);
+    });
+  });
+});
---
+++ src/day.8.ts
@@ -0,0 +1,84 @@
+import fs from "fs";
+
+import "./utils/array";
+import { gcd } from "./utils/number";
+
+type Node = {
+  label: string;
+  left: string;
+  right: string;
+};
+
+function parseInput() {
+  const sections = fs
+    .readFileSync("src/day.8.input.txt")
+    .toString()
+    .split("\n\n");
+
+  const instructions = sections[0].split("");
+
+  const network = sections[1]
+    .split("\n")
+    .filter((x) => x)
+    .map<Node>((x) => {
+      const [label, left, right] = x.match(/[A-Z0-9]{3}/g)!;
+
+      return {
+        label,
+        left,
+        right,
+      };
+    })
+    .reduce<Record<string, Node>>((lookup, node) => {
+      lookup[node.label] = node;
+      return lookup;
+    }, {});
+
+  return {
+    instructions,
+    network,
+  };
+}
+
+export function part1() {
+  const { instructions, network } = parseInput();
+
+  let position = "AAA";
+  let count = 0;
+
+  while (position !== "ZZZ") {
+    const direction = instructions[count % instructions.length];
+
+    position =
+      direction === "L" ? network[position].left : network[position].right;
+
+    count++;
+  }
+
+  return count;
+}
+
+export function part2() {
+  const { instructions, network } = parseInput();
+
+  return Object.values(network)
+    .filter((x) => x.label.endsWith("A"))
+    .map(({ label }) => {
+      let position = label;
+      let count = 0;
+
+      while (!position.endsWith("Z")) {
+        const direction = instructions[count % instructions.length];
+
+        position =
+          direction === "L" ? network[position].left : network[position].right;
+
+        count++;
+      }
+
+      return count;
+    })
+    .reduce((lcm, period) => {
+      return (lcm * period) / gcd(lcm, period);
+    }, 1);
+}

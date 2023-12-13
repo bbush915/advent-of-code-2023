6225643b21c2fb92b3b53d3d0b9f50a5c24c1b51
bbush915 <bushb@umich.edu>
2023-12-13 09:23:27 -0600

    feat: Day 12

--- src/day.12.ts
+++ src/day.12.ts
@@ -1,7 +1,6 @@
 import fs from "fs";
 
 import "./utils/array";
-import { clone } from "./utils/common";
 
 function parseInput(unfoldingFactor: number) {
   return fs
@@ -34,45 +33,36 @@ export function part2() {
 function getTotalArrangementCount(unfoldingFactor: number) {
   const conditionRecords = parseInput(unfoldingFactor);
 
-  return (
-    conditionRecords
-      //.slice(21, 22)
-      .map(({ spring, groups }, n) => {
-        // const comp = getArrangementCountBFS(
-        //   spring,
-        //   groups,
-        //   simplify(spring, groups)
-        // );
+  return conditionRecords
+    .map(({ spring, groups }) => {
+      const placements = simplify(spring, groups);
 
-        const placements = simplify(spring, groups);
-        const arrangementCounts = [new Array(placements[0].length).fill(1)];
+      const arrangementCounts = [new Array(placements[0].length).fill(1)];
 
-        for (let i = 1; i < placements.length; i++) {
-          arrangementCounts.push(new Array(placements[i].length).fill(0));
+      for (let i = 1; i < placements.length; i++) {
+        arrangementCounts.push(new Array(placements[i].length).fill(0));
 
-          for (let j = 0; j < placements[i].length; j++) {
-            for (let k = 0; k < placements[i - 1].length; k++) {
-              if (
-                placements[i][j] > placements[i - 1][k] + groups[i - 1] &&
-                !spring
-                  .slice(placements[i - 1][k] + groups[i - 1], placements[i][j])
-                  .includes("#")
-              ) {
-                arrangementCounts[i][j] += arrangementCounts[i - 1][k];
-              }
+        for (let j = 0; j < placements[i].length; j++) {
+          for (let k = 0; k < placements[i - 1].length; k++) {
+            if (
+              placements[i][j] > placements[i - 1][k] + groups[i - 1] &&
+              !spring
+                .slice(placements[i - 1][k] + groups[i - 1], placements[i][j])
+                .includes("#")
+            ) {
+              arrangementCounts[i][j] += arrangementCounts[i - 1][k];
             }
           }
         }
+      }
 
-        return arrangementCounts[arrangementCounts.length - 1].sum();
-      })
-      .sum()
-  );
+      return arrangementCounts[arrangementCounts.length - 1].sum();
+    })
+    .sum();
 }
 
 function simplify(spring: string[], groups: number[]) {
   const placements = new Array<Array<number>>();
-  const knownPlacements = new Array(groups.length).fill(false);
 
   for (let i = 0; i < groups.length; i++) {
     placements.push([]);
@@ -82,31 +72,25 @@ function simplify(spring: string[], groups: number[]) {
     }
   }
 
-  let iteration = 0;
+  simplifyHelper(spring, groups, placements);
 
-  do {
-    simplifyHelper(spring, groups, placements, iteration);
+  for (let n = 0; n < groups.length; n++) {
+    if (placements[n].length === 1) {
+      const index = placements[n][0];
 
-    for (let n = 0; n < groups.length; n++) {
-      if (!knownPlacements[n] && placements[n].length === 1) {
-        const index = placements[n][0];
+      if (index > 0) {
+        spring[index - 1] = ".";
+      }
 
-        if (index > 0) {
-          spring[index - 1] = ".";
-        }
+      for (let i = 0; i < groups[n]; i++) {
+        spring[index + i] = "#";
+      }
 
-        for (let i = 0; i < groups[n]; i++) {
-          spring[index + i] = "#";
-        }
-
-        if (index + groups[n] < spring.length - 1) {
-          spring[index + groups[n]] = ".";
-        }
-
-        knownPlacements[n] = true;
+      if (index + groups[n] < spring.length - 1) {
+        spring[index + groups[n]] = ".";
       }
     }
-  } while (iteration++ < 100);
+  }
 
   return placements;
 }
@@ -114,15 +98,9 @@ function simplify(spring: string[], groups: number[]) {
 function simplifyHelper(
   spring: string[],
   groups: number[],
-  placements: number[][],
-  iteration: number
+  placements: number[][]
 ) {
   for (let n = 0; n < groups.length; n++) {
-    // NOTE - Known placement.
-    if (placements[n].length === 1) {
-      continue;
-    }
-
     const validPlacements: number[] = [];
 
     placementLoop: for (let i = 0; i < placements[n].length; i++) {
@@ -186,282 +164,9 @@ function simplifyHelper(
         continue;
       }
 
-      if (iteration > 0) {
-        // NOTE - Can't place a piece after a known placement of a later group.
-
-        for (let j = n + 1; j < groups.length; j++) {
-          if (
-            placements[j].length === 1 &&
-            index + groups[n] >= placements[j][0]
-          ) {
-            continue placementLoop;
-          }
-        }
-
-        // NOTE - Must not prevent the next groups from being placed.
-
-        let threshold = index + groups[n];
-
-        for (let m = n + 1; m < groups.length; m++) {
-          if (placements[m].filter((x) => x > threshold).length === 0) {
-            continue placementLoop;
-          }
-
-          threshold = placements[m].filter((x) => x > threshold)[0];
-        }
-
-        // NOTE - Can't have damaged pieces between groups.
-
-        if (
-          n > 0 &&
-          spring
-            .slice(
-              placements[n - 1][placements[n - 1].length - 1] + groups[n - 1],
-              index
-            )
-            .includes("#")
-        ) {
-          continue;
-        }
-
-        if (
-          n < groups.length - 1 &&
-          spring.slice(index + groups[n], placements[n + 1][0]).includes("#")
-        ) {
-          continue;
-        }
-      }
-
       validPlacements.push(index);
     }
 
     placements[n] = validPlacements;
   }
-}
-
-function getArrangementCountDFS(
-  spring: string[],
-  groups: number[],
-  placements: number[][],
-  groupIndex: number,
-  springIndex: number,
-  mapping: number[]
-) {
-  if (groupIndex === groups.length) {
-    return 1;
-  }
-
-  let arrangementCount = 0;
-
-  for (let i = springIndex; i < spring.length; i++) {
-    if (spring[i] === ".") {
-      continue;
-    } else {
-      let canPlace = true;
-
-      for (let j = 0; j < groups[groupIndex]; j++) {
-        if (spring[i + j] === ".") {
-          canPlace = false;
-          break;
-        }
-      }
-
-      if (!canPlace) {
-        continue;
-      }
-    }
-
-    const submapping = clone(mapping);
-    submapping[groupIndex] = i;
-
-    if (isPossibleMapping(spring, groups, submapping)) {
-      arrangementCount += getArrangementCountDFS(
-        spring,
-        groups,
-        placements,
-        groupIndex + 1,
-        i + groups[groupIndex] + 1,
-        submapping
-      );
-    }
-  }
-
-  return arrangementCount;
-}
-
-function getArrangementCountBFS(
-  spring: string[],
-  groups: number[],
-  placements: number[][]
-) {
-  let mappings = getPossibleMappings(spring, groups, placements, 0, 0, []);
-
-  for (let n = 1; n < groups.length; n++) {
-    // console.log("Group: ", n, mappings.length);
-
-    const workingMappings: number[][] = [];
-
-    while (mappings.length > 0) {
-      const mapping = mappings.shift()!;
-
-      workingMappings.push(
-        ...getPossibleMappings(
-          spring,
-          groups,
-          placements,
-          n,
-          mapping[mapping.length - 1] + groups[mapping.length - 1] + 1,
-          mapping
-        )
-      );
-    }
-
-    mappings = workingMappings;
-  }
-
-  return mappings;
-}
-
-function getPossibleMappings(
-  spring: string[],
-  groups: number[],
-  placements: number[][],
-  groupIndex: number,
-  springIndex: number,
-  mapping: number[]
-) {
-  return placements[groupIndex]
-    .filter((x) => x >= springIndex)
-    .map((placement) => {
-      const submapping = clone(mapping);
-
-      submapping[groupIndex] = placement;
-
-      return submapping;
-    })
-    .filter((submapping) => isPossibleMapping(spring, groups, submapping));
-}
-
-function isPossibleMapping(
-  spring: string[],
-  groups: number[],
-  mapping: number[]
-) {
-  // NOTE - Populate proposed spring from mapping.
-
-  const proposedSpring = clone(spring);
-
-  let maxIndex = 0;
-  let partialPlacements = false;
-
-  for (let i = 0; i < groups.length; i++) {
-    const startIndex = mapping[i];
-
-    if (startIndex === undefined) {
-      partialPlacements = true;
-      break;
-    }
-
-    for (let j = 0; j < groups[i]; j++) {
-      const index = startIndex + j;
-
-      proposedSpring[index] = "#";
-      maxIndex = index + 1;
-    }
-  }
-
-  if (partialPlacements) {
-    while (
-      maxIndex < proposedSpring.length &&
-      proposedSpring[maxIndex] !== "?"
-    ) {
-      maxIndex++;
-    }
-  } else {
-    maxIndex = proposedSpring.length;
-  }
-
-  // NOTE - Validate proposed groups.
-
-  const proposedGroups = proposedSpring
-    .slice(0, maxIndex)
-    .join("")
-    .replace(/\?/g, ".")
-    .split(/\.+/)
-    .filter((x) => x)
-    .map((x) => x.length);
-
-  for (let i = 0; i < proposedGroups.length; i++) {
-    if (partialPlacements && i === proposedGroups.length - 1) {
-      if (proposedGroups[i] > groups[i]) {
-        return false;
-      } else {
-        continue;
-      }
-    }
-
-    if (proposedGroups[i] !== groups[i]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function isValidMapping(spring: string[], groups: number[], mapping: number[]) {
-  // NOTE - Populate proposed spring from mapping.
-
-  const proposedSpring = clone(spring);
-
-  let maxIndex = 0;
-  let partialPlacements = false;
-
-  for (let i = 0; i < groups.length; i++) {
-    const startIndex = mapping[i];
-
-    if (startIndex === undefined) {
-      partialPlacements = true;
-      break;
-    }
-
-    for (let j = 0; j < groups[i]; j++) {
-      const index = startIndex + j;
-
-      proposedSpring[index] = "#";
-      maxIndex = index + 1;
-    }
-  }
-
-  if (partialPlacements) {
-    while (
-      maxIndex < proposedSpring.length &&
-      proposedSpring[maxIndex] !== "?"
-    ) {
-      maxIndex++;
-    }
-  } else {
-    maxIndex = proposedSpring.length;
-  }
-
-  // NOTE - Validate proposed groups.
-
-  const proposedGroups = proposedSpring
-    .slice(0, maxIndex)
-    .join("")
-    .replace(/\?/g, ".")
-    .split(/\.+/)
-    .filter((x) => x)
-    .map((x) => x.length);
-
-  if (proposedGroups.length !== mapping.filter((x) => x !== null).length) {
-    return false;
-  }
-
-  for (let i = 0; i < proposedGroups.length; i++) {
-    if (proposedGroups[i] !== groups[i]) {
-      return false;
-    }
-  }
-
-  return true;
 }

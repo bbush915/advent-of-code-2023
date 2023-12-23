2505609fc116f40d3ff6bb07af4cee40ea62a2cc
bbush915 <bushb@umich.edu>
2023-12-22 21:43:30 -0500

    feat: Day 22

--- src/day.22.ts
+++ src/day.22.ts
@@ -1,8 +1,8 @@
 import fs from "fs";
 
 type Support = {
-  height: number;
-  brick: number;
+  z: number;
+  i: number;
 };
 
 function parseInput() {
@@ -11,108 +11,27 @@ function parseInput() {
     .toString()
     .split("\n")
     .filter((x) => x)
-    .map((x) => x.split("~").map((x) => x.split(",").map(Number)));
+    .map((x) => x.split("~").map((x) => x.split(",").map(Number)))
+    .sort((x, y) => Math.min(x[0][2], x[1][2]) - Math.min(y[0][2], y[1][2]));
 }
 
 export function part1() {
-  const bricks = parseInput().sort(
-    (x, y) => Math.min(x[0][2], x[1][2]) - Math.min(y[0][2], y[1][2])
-  );
-
-  const maxX = Math.max(
-    ...bricks.map((brick) => Math.max(brick[0][0], brick[1][0]))
-  );
-  const maxY = Math.max(
-    ...bricks.map((brick) => Math.max(brick[0][1], brick[1][1]))
-  );
-  const maxZ = Math.max(
-    ...bricks.map((brick) => Math.max(brick[0][2], brick[1][2]))
-  );
-
-  const tower: boolean[][][] = [];
-
-  for (let z = 0; z <= maxZ; z++) {
-    const layer: boolean[][] = [];
-
-    for (let x = 0; x <= maxX; x++) {
-      const row: boolean[] = [];
-
-      for (let y = 0; y <= maxY; y++) {
-        row.push(false);
-      }
-
-      layer.push(row);
-    }
-
-    tower.push(layer);
-  }
-
-  const supportLookup = new Map<number, Set<number>>();
-
-  const supports: Support[][] = [...new Array(maxX + 1)].map(() =>
-    [...new Array(maxY + 1)].map(() => ({ height: 0, brick: -1 }))
-  );
-
-  for (let brickIndex = 0; brickIndex < bricks.length; brickIndex++) {
-    const brick = bricks[brickIndex];
-
-    const dx = brick[1][0] - brick[0][0];
-    const dy = brick[1][1] - brick[0][1];
-    const dz = brick[1][2] - brick[0][2];
-
-    const possibleBrickSupports: Support[] = [];
-
-    for (let n = 0; n <= Math.max(Math.abs(dx), Math.abs(dy)); n++) {
-      const x = brick[0][0] + Math.sign(dx) * n;
-      const y = brick[0][1] + Math.sign(dy) * n;
-
-      possibleBrickSupports.push(supports[x][y]);
-    }
-
-    const maxBrickHeight =
-      Math.max(...possibleBrickSupports.map((x) => x.height)) + 1;
-
-    const brickSupports: Support[] = possibleBrickSupports.filter(
-      (x) => x.height === maxBrickHeight - 1
-    );
-
-    for (const support of brickSupports) {
-      if (!supportLookup.has(brickIndex)) {
-        supportLookup.set(brickIndex, new Set<number>());
-      }
-
-      supportLookup.get(brickIndex)!.add(support.brick);
-    }
-
-    for (
-      let n = 0;
-      n <= Math.max(Math.abs(dx), Math.abs(dy), Math.abs(dz));
-      n++
-    ) {
-      const x = brick[0][0] + Math.sign(dx) * n;
-      const y = brick[0][1] + Math.sign(dy) * n;
-      const z = brick[0][2] + Math.sign(dz) * n;
-
-      tower[maxBrickHeight + n][x][y] = true;
-
-      supports[x][y] = {
-        height: maxBrickHeight + n * Math.sign(dz),
-        brick: brickIndex,
-      };
-    }
-  }
+  const brickSupportLookup = getBrickSupportLookup();
 
   let count = 0;
 
-  for (let i = 0; i < bricks.length; i++) {
+  for (let i = 0; i < brickSupportLookup.size; i++) {
     let canDisintegrate = true;
 
-    for (let j = 0; j < bricks.length; j++) {
+    for (let j = 0; j < brickSupportLookup.size; j++) {
       if (i === j) {
         continue;
       }
 
-      if (supportLookup.get(j)?.has(i) && supportLookup.get(j)?.size === 1) {
+      if (
+        brickSupportLookup.get(j)?.has(i) &&
+        brickSupportLookup.get(j)?.size === 1
+      ) {
         canDisintegrate = false;
         break;
       }
@@ -127,73 +46,88 @@ export function part1() {
 }
 
 export function part2() {
-  const bricks = parseInput().sort(
-    (x, y) => Math.min(x[0][2], x[1][2]) - Math.min(y[0][2], y[1][2])
-  );
+  const brickSupportLookup = getBrickSupportLookup();
+
+  let count = 0;
+
+  for (let i = 0; i < brickSupportLookup.size; i++) {
+    const brickSupportLookup_ = clone(brickSupportLookup);
+
+    const disintegrations = [i];
+
+    while (disintegrations.length > 0) {
+      const disintegration = disintegrations.shift()!;
+
+      for (let j = 0; j < brickSupportLookup.size; j++) {
+        if (i === j) {
+          continue;
+        }
+
+        const brickSupports = brickSupportLookup_.get(j);
+
+        if (!brickSupports) {
+          continue;
+        }
+
+        if (brickSupports.has(disintegration)) {
+          brickSupports.delete(disintegration);
+
+          if (brickSupports.size === 0) {
+            disintegrations.push(j);
+
+            count++;
+          }
+        }
+      }
+    }
+  }
+
+  return count;
+}
+
+function getBrickSupportLookup() {
+  const bricks = parseInput();
 
   const maxX = Math.max(
     ...bricks.map((brick) => Math.max(brick[0][0], brick[1][0]))
   );
+
   const maxY = Math.max(
     ...bricks.map((brick) => Math.max(brick[0][1], brick[1][1]))
   );
-  const maxZ = Math.max(
-    ...bricks.map((brick) => Math.max(brick[0][2], brick[1][2]))
-  );
-
-  const tower: boolean[][][] = [];
-
-  for (let z = 0; z <= maxZ; z++) {
-    const layer: boolean[][] = [];
-
-    for (let x = 0; x <= maxX; x++) {
-      const row: boolean[] = [];
-
-      for (let y = 0; y <= maxY; y++) {
-        row.push(false);
-      }
-
-      layer.push(row);
-    }
-
-    tower.push(layer);
-  }
-
-  const supportLookup = new Map<number, Set<number>>();
 
   const supports: Support[][] = [...new Array(maxX + 1)].map(() =>
-    [...new Array(maxY + 1)].map(() => ({ height: 0, brick: -1 }))
+    [...new Array(maxY + 1)].map(() => ({ z: 0, i: -1 }))
   );
 
-  for (let brickIndex = 0; brickIndex < bricks.length; brickIndex++) {
-    const brick = bricks[brickIndex];
+  const brickSupportLookup = new Map<number, Set<number>>();
+
+  for (let i = 0; i < bricks.length; i++) {
+    const brick = bricks[i];
 
     const dx = brick[1][0] - brick[0][0];
     const dy = brick[1][1] - brick[0][1];
     const dz = brick[1][2] - brick[0][2];
 
-    const possibleBrickSupports: Support[] = [];
+    const possibleSupports: Support[] = [];
 
     for (let n = 0; n <= Math.max(Math.abs(dx), Math.abs(dy)); n++) {
       const x = brick[0][0] + Math.sign(dx) * n;
       const y = brick[0][1] + Math.sign(dy) * n;
 
-      possibleBrickSupports.push(supports[x][y]);
+      possibleSupports.push(supports[x][y]);
     }
 
-    const maxBrickHeight =
-      Math.max(...possibleBrickSupports.map((x) => x.height)) + 1;
+    const maxZ = Math.max(...possibleSupports.map((x) => x.z)) + 1;
 
-    const brickSupports: Support[] = possibleBrickSupports.filter(
-      (x) => x.brick >= 0 && x.height === maxBrickHeight - 1
-    );
-
-    for (const support of brickSupports) {
-      if (!supportLookup.has(brickIndex)) {
-        supportLookup.set(brickIndex, new Set<number>());
+    for (const support of possibleSupports.filter(
+      (support) => support.z === maxZ - 1
+    )) {
+      if (!brickSupportLookup.has(i)) {
+        brickSupportLookup.set(i, new Set<number>());
       }
 
-      supportLookup.get(brickIndex)!.add(support.brick);
+      brickSupportLookup.get(i)!.add(support.i);
     }
 
     for (
@@ -203,54 +137,22 @@ export function part2() {
     ) {
       const x = brick[0][0] + Math.sign(dx) * n;
       const y = brick[0][1] + Math.sign(dy) * n;
-      const z = brick[0][2] + Math.sign(dz) * n;
-
-      tower[maxBrickHeight + n][x][y] = true;
 
       supports[x][y] = {
-        height: maxBrickHeight + n * Math.sign(dz),
-        brick: brickIndex,
+        z: maxZ + n * Math.sign(dz),
+        i,
       };
     }
   }
 
-  let count = 0;
+  return brickSupportLookup;
+}
 
-  for (let i = 0; i < bricks.length; i++) {
-    const newSupportLookup = new Map(
-      [...supportLookup.entries()].map(([brickIndex, supports]) => [
-        brickIndex,
-        new Set(supports),
-      ])
-    );
-
-    const removals = [i];
-
-    while (removals.length > 0) {
-      const removal = removals.shift()!;
-
-      for (let j = 0; j < bricks.length; j++) {
-        if (i === j) {
-          continue;
-        }
-
-        const lookup = newSupportLookup.get(j);
-
-        if (!lookup) {
-          continue;
-        }
-
-        if (lookup.has(removal)) {
-          lookup.delete(removal);
-
-          if (lookup.size === 0) {
-            count++;
-            removals.push(j);
-          }
-        }
-      }
-    }
-  }
-
-  return count;
+function clone(brickSupportLookup: Map<number, Set<number>>) {
+  return new Map(
+    [...brickSupportLookup.entries()].map(([i, brickSupports]) => [
+      i,
+      new Set(brickSupports),
+    ])
+  );
 }
